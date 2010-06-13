d065a5b5825fdde2d10fc5af3916dd90052dd4c7
tajpelc@gmail.com <taj@webcrawler.local>
2010-06-13 05:49:38 +0200

    Lots of changes

---
+++ js/criteria.js
@@ -0,0 +1,17 @@
+/**
+ * On document load
+ */
+$(document).ready(function(){
+    $(function() {
+        $("#sortable").sortable({
+            opacity: 0.9,
+        });
+        $("#sortable").disableSelection();
+    });
+
+    $( "#sortable" ).bind( "sortupdate", function(event, ui) {
+        $.get(location.href, {
+            criteriaOrder: $(this).sortable('toArray').toString(),
+        });
+    });
+});
