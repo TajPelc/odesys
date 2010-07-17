db3cfe56f4fc8b63896e50da27fc30230a86515a
tajpelc@gmail.com <taj@webcrawler.local>
2010-07-17 03:43:56 +0200

    Text changed on project create page.

--- protected/views/project/create.php
+++ protected/views/project/create.php
@@ -1,4 +1,4 @@
 <?php $this->pageTitle = Yii::app()->name . ' / ' . ($model->isNewRecord ? 'Create project' : 'Edit project details'); ?>
 <h1><?php if( $model->isNewRecord) { ?>Create Project<?php } else { ?>Project details<?php }?></h1>
-<p><?php if( $model->isNewRecord) { ?>Create a project by filling the form to start the decision support process.<?php } else { ?>Modify values in the form to edit active project's details.<?php }?></p>
+<p><?php if( $model->isNewRecord) { ?>Create a project by filling in the form. Choose a title and write a short description about the nature of your decision problem, your goals, expectations, notes or other.<?php } else { ?>Modify values in the form to edit active project's details.<?php }?></p>
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
