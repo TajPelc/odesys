cd45c162cad6c3478ef6a3efde2c19712ba6f8ef
Frenk Ten <frenk.ten@gmail.com>
2010-10-14 00:02:26 +0200

    Added sidebar functionality

--- protected/views/criteria/create.php
+++ protected/views/criteria/create.php
@@ -1,19 +1,23 @@
-<?php $this->pageTitle = Yii::app()->name . ' / ' . CHtml::encode($Project->title) . ' / ' . ' Criteria'; ?>
 <h1>Criteria</h1>
-<p>Add between 2 and 10 criteria for rating the alternatives. Provide best (most desired) and worst (least desired) values. Set the best and worst values low or high enough that no alternative that you are considering falls under or over this marign.</p>
-<p>Sample criteria: Max speed / <em>Worst:</em> 140kph / <em>Best:</em> 220kph, Price / <em>Worst:</em> 25.000€ / <em>Best:</em> 10.000€</p>
-<?php echo $this->renderPartial('_form', array('model' => $model, 'Project' => $Project)); ?>
-
-<p>You may drag each criteria up or down to rearrange the list by priority. The most important criteria to you is at the top. Criteria of lowest importance at the bottom.</p>
-<?php echo Chtml::button('Add criteria', array('id' => 'create', 'class' => 'hidden')); ?>
-<?php $Criteria = $Project->findCriteriaByPriority(); ?>
-<div id="criteria">
-    <?php if(Common::isArray($Criteria)) {?>
-        <ul id="sortable">
-            <?php foreach($Criteria as $C) {?>
-                <li id="criteria_<?php echo $C->criteria_id; ?>" class="movable"><span><?php echo CHtml::encode($C->title); ?></span><?php echo CHtml::link('delete', array('delete', 'criteria_id' => $C->criteria_id), array('class' => 'delete')); ?><?php echo CHtml::link('edit', array('create', 'criteria_id' => $C->criteria_id), array('class' => 'edit')); ?></li>
-            <?php }?>
-        </ul>
-    <?php }?>
+<div id="content">
+    <?php $this->pageTitle = Yii::app()->name . ' / ' . CHtml::encode($Project->title) . ' / ' . ' Criteria'; ?>
+    <?php echo $this->renderPartial('_form', array('model' => $model, 'Project' => $Project)); ?>
+    
+    <p>You may drag each criteria up or down to rearrange the list by priority. The most important criteria to you is at the top. Criteria of lowest importance at the bottom.</p>
+    <?php echo Chtml::button('Add criteria', array('id' => 'create', 'class' => 'hidden')); ?>
+    <?php $Criteria = $Project->findCriteriaByPriority(); ?>
+    <div id="criteria">
+        <?php if(Common::isArray($Criteria)) {?>
+            <ul id="sortable">
+                <?php foreach($Criteria as $C) {?>
+                    <li id="criteria_<?php echo $C->criteria_id; ?>" class="movable"><span><?php echo CHtml::encode($C->title); ?></span><?php echo CHtml::link('delete', array('delete', 'criteria_id' => $C->criteria_id), array('class' => 'delete')); ?><?php echo CHtml::link('edit', array('create', 'criteria_id' => $C->criteria_id), array('class' => 'edit')); ?></li>
+                <?php }?>
+            </ul>
+        <?php }?>
+    </div>
+    <?php echo CHtml::link('Continue', array('alternative/create'), array('class' => 'button right' . (count($Project->criteria) > 1 ? '' : ' hide'))); ?>
 </div>
-<?php echo CHtml::link('Continue', array('alternative/create'), array('class' => 'button right' . (count($Project->criteria) > 1 ? '' : ' hide'))); ?>
+<div id="sidebar">
+    <p>Add between 2 and 10 criteria for rating the alternatives. Provide best (most desired) and worst (least desired) values. Set the best and worst values low or high enough that no alternative that you are considering falls under or over this marign.</p>
+    <p>Sample criteria: Max speed / <em>Worst:</em> 140kph / <em>Best:</em> 220kph, Price / <em>Worst:</em> 25.000€ / <em>Best:</em> 10.000€</p>
+</div>
--- protected/views/project/index.php
+++ protected/views/project/index.php
@@ -1,29 +1,31 @@
-<?php $this->pageTitle = Yii::app()->name . ' / ' . ' My Projects'; ?>
-<h1>My Projects</h1>
-<?php if(!empty($Projects)) { ?>
-<p>Click on an existing project to make it active (appear in the left sidebar).</p>
-<ul class="projects">
-    <?php foreach($Projects as $P) {?>
-        <?php $active = (false !== $Project && $P->project_id === $Project->project_id);?>
-        <?php $Alternatives = $P->alternatives; ?>
-        <li<?php if($active) { ?> class="active"<?php }?>>
-            <?php if($active) { ?>
-                <span><?php echo CHtml::encode($P->title)?></span>
-                <?php echo CHtml::link(CHtml::encode($P->title), array('view', 'project_id' => $P->project_id), array('style' => 'display: none;', 'title' => CHtml::encode($P->title))); ?>
-            <?php } else { ?>
-                <?php echo CHtml::link(CHtml::encode($P->title), array('view', 'project_id' => $P->project_id), array('title' => CHtml::encode($P->title))); ?>
-            <?php }?>
-            <?php if(empty($Alternatives)){ ?>
-                <p>No alternatives defined</p>
-            <?php } else { ?>
-                <ul>
-                    <?php foreach($Alternatives as $A) {?>
-                        <li><?php echo CHtml::encode($A->title);?></li>
-                    <?php }?>
-                </ul>
-            <?php }?>
-        </li>
+<div id="content">
+    <?php $this->pageTitle = Yii::app()->name . ' / ' . ' My Projects'; ?>
+    <h1>My Projects</h1>
+    <?php if(!empty($Projects)) { ?>
+    <p>Click on an existing project to make it active (appear in the left sidebar).</p>
+    <ul class="projects">
+        <?php foreach($Projects as $P) {?>
+            <?php $active = (false !== $Project && $P->project_id === $Project->project_id);?>
+            <?php $Alternatives = $P->alternatives; ?>
+            <li<?php if($active) { ?> class="active"<?php }?>>
+                <?php if($active) { ?>
+                    <span><?php echo CHtml::encode($P->title)?></span>
+                    <?php echo CHtml::link(CHtml::encode($P->title), array('view', 'project_id' => $P->project_id), array('style' => 'display: none;', 'title' => CHtml::encode($P->title))); ?>
+                <?php } else { ?>
+                    <?php echo CHtml::link(CHtml::encode($P->title), array('view', 'project_id' => $P->project_id), array('title' => CHtml::encode($P->title))); ?>
+                <?php }?>
+                <?php if(empty($Alternatives)){ ?>
+                    <p>No alternatives defined</p>
+                <?php } else { ?>
+                    <ul>
+                        <?php foreach($Alternatives as $A) {?>
+                            <li><?php echo CHtml::encode($A->title);?></li>
+                        <?php }?>
+                    </ul>
+                <?php }?>
+            </li>
+        <?php }?>
+    </ul>
     <?php }?>
-</ul>
-<?php }?>
-<p><?php echo CHtml::link('Create a new project', array('create', 'createNew' => '1'), array('class' => 'button')); ?></p>
+    <p><?php echo CHtml::link('Create a new project', array('create', 'createNew' => '1'), array('class' => 'button')); ?></p>
+</div>
