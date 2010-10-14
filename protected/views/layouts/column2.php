8188daa6b8f8541024068bbd6ee6c2cd80876583
tajpelc@gmail.com <taj@webcrawler.local>
2010-10-14 23:08:36 +0200

    Column 2 layout now properly lists project name and fetch url.

--- protected/views/layouts/column2.php
+++ protected/views/layouts/column2.php
@@ -1,9 +1,10 @@
 <?php $this->beginContent('application.views.layouts.main'); ?>
+<?php $Project = Project::getActive(); ?>
 <div id="projectUrl">
-    <h1>Project NAME HIER!</h1>
+    <h1><?php echo CHTML::encode($Project->title);?></h1>
     <form action="">
         <fieldset>
-            <input class="ui-widget input ui-corner-all ui-widget-content" type="text" name="dummy" value="http://www.SEX.pistacija.noge.dolge.zabe.mmmm" />
+            <input class="ui-widget input ui-corner-all ui-widget-content" type="text" name="dummy" value="<?php echo Yii::app()->createAbsoluteUrl('project/set',array('i'=>$Project->url));?>" />
             <input type="button" value="Add to bookmarks!" name="addToBookMarks" class="hidden ui-button ui-widget ui-state-default ui-corner-all">
         </fieldset>
     </form>
