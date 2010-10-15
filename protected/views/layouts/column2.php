a2aabb28f18ca9f88c97019663d5970674e5b79f
tajpelc@gmail.com <taj@webcrawler.local>
2010-10-15 22:28:29 +0200

    Changed the input field to readonly

--- protected/views/layouts/column2.php
+++ protected/views/layouts/column2.php
@@ -5,7 +5,7 @@
     <h1><?php echo CHtml::encode($Project->title);?></h1>
     <form action="">
         <fieldset>
-            <input id="projectUrl_input" class="ui-widget input ui-corner-all ui-widget-content" type="text" name="dummy" value="<?php echo Yii::app()->createAbsoluteUrl('project/set',array('i'=>$Project->url));?>" />
+            <input id="projectUrl_input" class="ui-widget input ui-corner-all ui-widget-content" type="text" name="dummy" value="<?php echo Yii::app()->createAbsoluteUrl('project/set',array('i'=>$Project->url));?>" readonly="readonly" />
             <?php echo CHtml::link('Add to bookmarks!', Yii::app()->createAbsoluteUrl('project/set',array('i'=>$Project->url)), array('class' => 'button', 'id' => 'addToBookMarks', 'title' => 'ODESYS: '.CHtml::encode($Project->title))); ?>
             <span class="span">Copy url or -></span>
         </fieldset>
