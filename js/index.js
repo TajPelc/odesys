94a2ed95c15653839829b68fe998f5994b893726
frenk <frenk.ten@gmail.com>
2010-10-16 01:45:14 +0200

    index page pimped

--- js/index.js
+++ js/index.js
@@ -10,4 +10,40 @@ $(document).ready(function(){
         projectOverlay($(this).attr('href'), true);
         event.preventDefault();
     });
+
+    var olHeight = $('#content ol').height();
+
+    $('#content li').hide().parent().height(olHeight);
+
+    setTimeout(function(){
+        $('#content li:eq(0)').fadeIn(500);
+    }, 1000);
+    setTimeout(function(){
+        $('#content li:eq(1)').fadeIn(500);
+    }, 3000);
+    setTimeout(function(){
+        $('#content li:eq(2)').fadeIn(500);
+    }, 5000);
+    setTimeout(function(){
+        $('#content li:eq(3)').fadeIn(500);
+    }, 7000);
+    setTimeout(function(){
+        $('#content li:eq(4)').fadeIn(500);
+    }, 9000);
+    setTimeout(function(){
+        $('#content li:eq(5)').fadeIn(500);
+    }, 11000);
+    setTimeout(function(){
+        $('#content li:eq(6)').fadeIn(500);
+    }, 13000);
+    setTimeout(function(){
+        $('#content #create').animate({
+            opacity: 0.2
+        }, 300).addClass('glow')
+    }, 15000);
+    setTimeout(function(){
+        $('#content #create').animate({
+            opacity: 1
+        }, 300).removeClass('glow')
+    }, 15300);
 });
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,8 +4,8 @@
 <h1>Welcome to ODESYS</h1>
 <h2>The on-line decision support system.</h2>
 <p>ODESYS helps you reach a better decision.</p>
-<p>Say for example that you are buying a new car and choosing among three different models. ODESYS can guide you to the right model for you by providing a graphical overview of each model's characteristics in just a few easy steps.</p>
-<ul>
+<p>Say for example that you are buying a new car and choosing among three different models. ODESYS can guide you to the right model for you by providing a graphical overview of each model's characteristics in just <b>a few easy steps</b>:</p>
+<ol>
     <li>Create a project</li>
     <li>Define criteria and order them from most to least important (Example: Price, NCAP Rating, Engine Power, Features)</li>
     <li>Define alternatives between which you are choosing (Example: Ford, VW, Audi, Fiat)</li>
@@ -13,6 +13,6 @@
     <li>Compare alternatives on the graph</li>
     <li>Add/remove criteria or alternatives, reevaluate alternatives or change the importance of different criteria</li>
     <li>Make a better decision armed with additional information provided by ODESYS</li>
-</ul>
+</ol>
 <p>ODESYS is highly flexible and is suitable for a wide variety of decision problems.</p>
 <?php if(!Project::isProjectActive()){ ?><p><?php echo CHtml::link('Start by creating a new project', array('project/create'), array('class' => 'button', 'id' => 'create')); ?></p><?php }?>
