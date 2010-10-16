94db8ecaf21dd8eb238930f425d17b6216b2b112
frenk <frenk.ten@gmail.com>
2010-10-16 17:09:58 +0200

    Fixed bugz0r

--- protected/components/views/tabs.php
+++ protected/components/views/tabs.php
@@ -2,7 +2,7 @@
     <?php $this->widget('zii.widgets.CMenu',array(
        'items'=>array(
             array('label' => 'Home',                                'url'=>array('/site/index')),
-            array('label' => $project->title,                       'url'=>array('/project/details'),       'visible' => Project::isProjectActive(), 'active' => $checkUrl, 'linkOptions' => array('id' => (Project::isProjectActive() && !$checkUrl ? 'display-project-warning' : 'project-disabled'))),
+            array('label' => ($project instanceof Project ? $project->title : 'Project'),                       'url'=>array('/project/details'),       'visible' => Project::isProjectActive(), 'active' => $checkUrl, 'linkOptions' => array('id' => (Project::isProjectActive() && !$checkUrl ? 'display-project-warning' : 'project-disabled'))),
             array('label' => 'Create a new project',                'url'=>array('/project/create'),        'visible' => Project::isProjectActive(),                        'linkOptions' => array('id' => 'createNewProject')),
             array('label' => 'My projects',                         'url'=>array('/project/index'),         'visible' => !Yii::app()->user->isGuest),
             array('label' => 'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'),           'visible' => !Yii::app()->user->isGuest),
