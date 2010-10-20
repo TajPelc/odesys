2e0339da9c72847526c6cf2e184087a95cf3272e
tajpelc@gmail.com <taj@webcrawler.local>
2010-10-20 23:00:33 +0200

    Changed the text on the front page.

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -2,17 +2,24 @@
 <?php if(Project::isProjectActive()) { ?>
 <?php } ?>
 <h1>Welcome to ODESYS</h1>
-<h2>The on-line decision support system.</h2>
-<p>ODESYS helps you reach a better decision.</p>
-<p>Say for example that you are buying a new car and choosing among three different models. ODESYS can guide you to the right model for you by providing a graphical overview of each model's characteristics in just <b>a few easy steps</b>:</p>
+<dl>
+    <dt><em>What is it?</em></dt>
+    <dd>ODESYS is a simple online decision support system (<a href="http://en.wikipedia.org/wiki/Decision_support_system" title="Decision Support System">DSS</a>) with a friendly graphical user interface.</dd>
+    <dt>How does it work?</dt>
+    <dd>It seamlessly guides you through the process of creating a decision model, the evaluation of alternatives and results analysis. Armed with new knowledge the hard part is still up to you. Making a choice.</dd>
+    <dt>What may I use it for?</dt>
+    <dd>A wide variety of decision problems. A basic understanding of <a href="http://en.wikipedia.org/wiki/Decision_theory" title="Decision making">decision theory</a> is wanted, but not required. Try it out!</dd>
+</dl>
+<h2>How to use odesys?</h2>
+<p>First gain a good understanding of your decison problem. You may then proceed to:</p>
 <ol>
-    <li><p>Create a project</p></li>
-    <li><p>Define criteria and order them from most to least important (Example: Price, NCAP Rating, Engine Power, Features)</p></li>
-    <li><p>Define alternatives between which you are choosing (Example: Ford, VW, Audi, Fiat)</p></li>
-    <li><p>Evalute each alternative by the defined criteria using simple sliders</p></li>
-    <li><p>Compare alternatives on the graph</p></li>
-    <li><p>Add/remove criteria or alternatives, reevaluate alternatives or change the importance of different criteria</p></li>
-    <li><p>Make a better decision armed with additional information provided by ODESYS</p></li>
+    <li><p>Create a new project</p></li>
+    <li><p>Define criteria and order them from most to least important</p></li>
+    <li><p>Define alternatives between which you are choosing</p></li>
+    <li><p>Evalute each alternative by the defined criteria</p></li>
+    <li><p>Analyse the results using a graphical interpretation of results</p></li>
+    <li><p>Add/remove criteria or alternatives, reevaluate alternatives or change the order (importance) of criteria</p></li>
+    <li><p>Make a better decision</p></li>
 </ol>
-<p>ODESYS is highly flexible and is suitable for a wide variety of decision problems.</p>
+<p></p>
 <?php if(!Project::isProjectActive()){ ?><p><?php echo CHtml::link('Start by creating a new project', array('project/create'), array('class' => 'button', 'id' => 'create')); ?></p><?php }?>
