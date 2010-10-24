bd72d178bf123136955821e38371726eed11b013
Taj Pelc <tajpelc@gmail.com>
2010-10-24 15:05:46 +0200

    Added google analytics tracking.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -23,6 +23,17 @@
     <?php if('results/display' == $this->getRoute() ) { ?><!--[if IE]><script language="javascript" type="text/javascript" src="/js/jqplot/excanvas.js"></script><![endif]--><?php } ?>
 
     <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-19289535-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
 </head>
 
 <body>
