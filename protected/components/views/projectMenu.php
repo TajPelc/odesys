312aeb9bfbeca3f507da133656e340c7abb362ba
frenk <frenk@PAMELA-VM.lan>
2011-04-04 15:02:14 +0200

    Progress bar

--- protected/components/views/projectMenu.php
+++ protected/components/views/projectMenu.php
@@ -9,11 +9,11 @@
                     <?php foreach($menu as $id => $menuItem) {?>
                         <?php $label = $i . '. ' . $menuItem['label']; ?>
                         <?php if($menuItem['route'][0] === $currentRoute) { ?>
-                            <li><span id="<?php echo $id; ?>" class="<?php if($menuItem['enabled']) {?>selected<?php } else { ?>restricted<?php }?>"><?php echo CHtml::encode($label); ?></span><span class="loadingBar"></span><span class="loadingBarEnd">&nbsp;</span></li>
+                            <li><span id="<?php echo $id; ?>" class="<?php if($menuItem['enabled']) {?>selected<?php } else { ?>restricted<?php }?>"><?php echo CHtml::encode($label); ?></span><span class="loadingBar"></span></li>
                         <?php } elseif (!$menuItem['enabled']) { ?>
                             <li><span id="<?php echo $id; ?>" class="<?php if($menuItem['enabled']) {?>selected<?php } else { ?>restricted<?php }?>"><?php echo CHtml::encode($label); ?></span></li>
                         <?php } else { ?>
-                            <li><?php echo CHtml::link(CHtml::encode($label), $menuItem['route'], array('title' => CHtml::encode($label), 'id' => $id)); ?><span class="loadingBar">&nbsp;</span><span class="loadingBarEnd">&nbsp;</span></li>
+                            <li><?php echo CHtml::link(CHtml::encode($label), $menuItem['route'], array('title' => CHtml::encode($label), 'id' => $id)); ?><span class="loadingBar end">&nbsp;</span></li>
                         <?php } ?>
                         <?php $i++;?>
                     <?php } ?>
