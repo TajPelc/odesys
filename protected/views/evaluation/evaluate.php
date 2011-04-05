7ef2ff4c2e91ce1348fff8376b6131ba7e7c1f1f
frenk <frenk@PAMELA-VM.lan>
2011-04-05 21:06:28 +0200

    Evaluation fixes

--- protected/views/evaluation/evaluate.php
+++ protected/views/evaluation/evaluate.php
@@ -3,27 +3,25 @@
     <p>Fill out the statements by moving the sliders to the appropriate location.</p>
 
     <h2><b><?php echo CHtml::encode($Criteria->title); ?></b> for</h2>
-    <div>
-        <form id="evaluation" method="post" enctype="application/x-www-form-urlencoded">
-            <ul>
-                <?php foreach($Project->alternatives as $Alternative) { ?>
-                <?php $Evaluation = isset($eval[$Alternative->alternative_id]) ? $eval[$Alternative->alternative_id] : false; ?>
-                <li<?php echo ((bool)$Evaluation ? ' class="saved"' : ''); ?>>
-                    <h3><b><?php echo CHtml::encode($Alternative->title)?></b> is</h3>
-                    <div>
-                        <span class="worst">the worst</span>
-                        <select id="<?php echo 'eval-'. $Alternative->alternative_id . '-' . $Criteria->criteria_id; ?>" name="<?php echo 'eval['. $Alternative->alternative_id . '][' . $Criteria->criteria_id . ']'; ?>">
-                        <?php for($i = 0; $i <= 100; $i++) { ?>
-                            <option value="<?php echo $i; ?>" <?php echo (((bool)$Evaluation && $Evaluation->grade == $i) ? 'selected="selected"' : ''); ?>><?php echo $i; ?></option>
-                        <?php }?>
-                        </select>
-                        <span class="best">the best</span>
-                    </div>
-                </li>
-                <?php } ?>
-            </ul>
-        </form>
-    </div>
+    <form id="evaluation" method="post" enctype="application/x-www-form-urlencoded">
+        <ul>
+            <?php foreach($Project->alternatives as $Alternative) { ?>
+            <?php $Evaluation = isset($eval[$Alternative->alternative_id]) ? $eval[$Alternative->alternative_id] : false; ?>
+            <li<?php echo ((bool)$Evaluation ? ' class="saved"' : ''); ?>>
+                <h3><b><?php echo CHtml::encode($Alternative->title)?></b> is</h3>
+                <div>
+                    <span class="worst">the worst</span>
+                    <select id="<?php echo 'eval-'. $Alternative->alternative_id . '-' . $Criteria->criteria_id; ?>" name="<?php echo 'eval['. $Alternative->alternative_id . '][' . $Criteria->criteria_id . ']'; ?>">
+                    <?php for($i = 0; $i <= 100; $i++) { ?>
+                        <option value="<?php echo $i; ?>" <?php echo (((bool)$Evaluation && $Evaluation->grade == $i) ? 'selected="selected"' : ''); ?>><?php echo $i; ?></option>
+                    <?php }?>
+                    </select>
+                    <span class="best">the best</span>
+                </div>
+            </li>
+            <?php } ?>
+        </ul>
+    </form>
     <ul>
         <li><a class="previous" href="#">Previous</a></li>
         <li>Criteria 5 of 9</li>
