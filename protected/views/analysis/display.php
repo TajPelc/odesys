7fecd7758a70f8dcc5dabc6291ee5789a722a1e0
frenk <frenk@PAMELA-VM.lan>
2011-04-11 15:18:14 +0200

    Prepared accordion

--- protected/views/analysis/display.php
+++ protected/views/analysis/display.php
@@ -8,7 +8,25 @@
             <p>Sidebar</p>
         </div>
     </div>
-    <h3><a href="#">Another graph</a></h3>
+    <h3><a href="#">Pie chart</a></h3>
+    <div>
+        <div class="content">
+            <p>Here be another graph...</p>
+        </div>
+        <div class="sidebar">
+            <p>and sidebar..</p>
+        </div>
+    </div>
+    <h3><a href="#">Alternative analysis</a></h3>
+    <div>
+        <div class="content">
+            <p>Here be another graph...</p>
+        </div>
+        <div class="sidebar">
+            <p>and sidebar..</p>
+        </div>
+    </div>
+    <h3><a href="#">Statistics</a></h3>
     <div>
         <div class="content">
             <p>Here be another graph...</p>
