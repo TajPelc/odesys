2f53a1e5a04d5dbfadfa04611f00f5f5dd8c64af
frenk <frenk@PAMELA-VM.lan>
2011-04-14 23:08:14 +0200

    Fixes to index page

--- js/index/index.js
+++ js/index/index.js
@@ -12,7 +12,7 @@ Index.Rotation = function() {
     //Get the first image and display it (gets set to full opacity)
     $('#sidebar ul li.show').css({opacity: 1});
 
-    $('#sidebar ol li:first p').switchClass('', 'glow', 1000);
+    $('#content ol li:first p').switchClass('', 'glow', 1000);
 
     //Call the rotator function to run the slideshow, 6000 = change to next image after 6 seconds
     setInterval('Index.Rotate()',5000);
@@ -35,10 +35,10 @@ Index.Rotate = function() {
 
 
     //Highlight list
-    var currentList = $('#sidebar ol li.current');
+    var currentList = $('#content ol li.current');
 
     //Get next list
-    var nextList = ((currentList.next().length) ? ((currentList.next().hasClass('glow')) ? $('#sidebar ol li:first') : currentList.next()) : $('#sidebar ol li:first'));
+    var nextList = ((currentList.next().length) ? ((currentList.next().hasClass('glow')) ? $('#content ol li:first') : currentList.next()) : $('#content ol li:first'));
 
     //Set glow
     nextList.addClass('current').children('p').switchClass('', 'glow', 500);
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -2,6 +2,16 @@
     <?php $this->pageTitle=Yii::app()->name . 'ODESYS: Online Decision Support System'; ?>
     <?php if(Project::isProjectActive()) { ?>
     <?php } ?>
+    <h2>How to use odesys?</h2>
+    <p>First gain a good understanding of your decison problem. You may then proceed to:</p>
+    <ol>
+        <li class="current"><p>Start by signing in with Facebook and creating a new project</p></li>
+        <li><p>Define alternatives among which you are choosing</p></li>
+        <li><p>Define criteria and order them from most to least important</p></li>
+        <li><p>Evalute each alternative by the defined criteria</p></li>
+        <li><p>Analyse the results using our graphical method</p></li>
+        <li><p>Make a better decision</p></li>
+    </ol>
     <dl>
         <dt><em>What is it?</em></dt>
         <dd>ODESYS is a simple online decision support system (<a href="http://en.wikipedia.org/wiki/Decision_support_system" title="Decision Support System">DSS</a>) with a friendly graphical user interface.</dd>
@@ -32,15 +42,5 @@
             <img src="/images/pictures/introduction_6.png" title=""  alt="pic3" />
         </li>
     </ul>
-    <h2>How to use odesys?</h2>
-    <p>First gain a good understanding of your decison problem. You may then proceed to:</p>
-    <ol>
-        <li class="current"><p>Start by signing in with Facebook and creating a new project</p></li>
-        <li><p>Define alternatives among which you are choosing</p></li>
-        <li><p>Define criteria and order them from most to least important</p></li>
-        <li><p>Evalute each alternative by the defined criteria</p></li>
-        <li><p>Analyse the results using our graphical method</p></li>
-        <li><p>Make a better decision</p></li>
-    </ol>
     <?php if(!Project::isProjectActive()){ ?><p><?php echo CHtml::link('Start by creating a new project', array('criteria/create'), array('class' => 'button', 'id' => 'create')); ?></p><?php }?>
 </div>
