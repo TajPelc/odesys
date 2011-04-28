bf1b06475964415c54c7d4e96c629c727e70bac4
frenk <frenk@PAMELA-VM.lan>
2011-04-28 15:21:57 +0200

    Prepared sharing page

--- protected/modules/decision/views/sharing/index.php
+++ protected/modules/decision/views/sharing/index.php
@@ -6,40 +6,36 @@
 </div>
 
 <div id="content">
-    <ul id="privacy">
-        <li>
-            <dl>
-                <dt>Privacy settings</dt>
-                <dd>
-                    <form action="" method="post">
-                        <fieldset>
-                            <label>This decision may be viewed by</label>
-                            <select name="privacy_decision">
-                                <option value="everyone">Everyone</option>
-                                <option value="friends_only">Friends only</option>
-                                <option value="only_me">Only me</option>
-                            </select>
-                        </fieldset>
-                    </form>
-                </dd>
-                <dd>
-                    <form action="" method="post">
-                        <fieldset>
-                            <label>Comments and suggestions may be posted by</label>
-                            <select name="privacy_comments">
-                                <option value="everyone">Everyone</option>
-                                <option value="friends_only">Friends only</option>
-                                <option value="only_me">Only me</option>
-                            </select>
-                        </fieldset>
-                    </form>
-                </dd>
-            </dl>
-        </li>
-    </ul>
-    <ul id="content-nav">
-        <li class="prev"><?php echo CHtml::link('Previous', array('/decision/analysis', 'decisionId' => $this->Decision->project_id, 'decisionId' => $this->Decision->project_id, 'label' => $this->Decision->label)); ?></li>
-        <li class="next"><a href="#">Publish</a></li>
-    </ul>
+    <form action="" method="post">
+        <label class="author" for="comment_new">Description:</label>
+        <textarea name="description_new" id="comment_new" rows="5" cols="63"></textarea>
+        <ul id="privacy">
+            <li>
+                <dl>
+                    <dt>Privacy settings</dt>
+                    <dd>
+                        <label>This decision may be viewed by</label>
+                        <select name="privacy_decision">
+                            <option value="everyone">Everyone</option>
+                            <option value="friends_only">Friends only</option>
+                            <option value="only_me">Only me</option>
+                        </select>
+                    </dd>
+                    <dd>
+                        <label>Comments and suggestions may be posted by</label>
+                        <select name="privacy_comments">
+                            <option value="everyone">Everyone</option>
+                            <option value="friends_only">Friends only</option>
+                            <option value="only_me">Only me</option>
+                        </select>
+                    </dd>
+                </dl>
+            </li>
+        </ul>
+        <ul id="content-nav">
+            <li class="prev"><?php echo CHtml::link('Previous', array('/decision/analysis', 'decisionId' => $this->Decision->project_id, 'decisionId' => $this->Decision->project_id, 'label' => $this->Decision->label)); ?></li>
+            <li class="next"><input type="submit" name="publish" value="Publish" /></li>
+        </ul>
+    </form>
 </div>
 <div id="sidebar"></div>
