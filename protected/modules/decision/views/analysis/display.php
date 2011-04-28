de14bc80b4f7755fbac25b8c7e3088a6201c176b
frenk <frenk@PAMELA-VM.lan>
2011-04-29 01:28:40 +0200

    Fixes to analysis

--- protected/modules/decision/views/analysis/display.php
+++ protected/modules/decision/views/analysis/display.php
@@ -34,9 +34,16 @@ Graph.Data = <?php echo json_encode($eval); ?>;
 </div>
 
 <div id="accordion">
-    <h3>Results</h3>
-    <p>This graph shows you score values for all your alternatives. Best scoring alternative is at the top, having 100 points. All other alternative's scores are displayed relative to the winning alternative.</p>
     <div>
+        <h3>Results</h3>
+        <p>This graph shows you score values for all your alternatives. Best scoring alternative is at the top, having 100 points. All other alternative's scores are displayed relative to the winning alternative.</p>
+        <div class="sidebar help">
+            <h4>But which one is best?</h4>
+            <p>This is the hard part. Although the first alternative is usually the best, it's not a general rule. Use the detailed comparison below to see how they really compare.</p>
+            <h4>Changed your mind?</h4>
+            <p class="l">You may return to modify criteria, alternatives or evaluation at any time.</p>
+            <div class="last"></div>
+        </div>
         <div id="score" class="content">
             <table class="alternatives">
             <?php foreach($bestAlternatives as $A) { ?>
@@ -61,18 +68,34 @@ Graph.Data = <?php echo json_encode($eval); ?>;
                 </tr>
             </table>
         </div>
-        <div class="sidebar help">
-            <h4>But which one is best?</h4>
-            <p>This is the hard part. Although the first alternative is usually the best, it's not a general rule. Use the detailed comparison below to see how they really compare.</p>
-            <h4>Changed your mind?</h4>
-            <p class="l">You may return to modify criteria, alternatives or evaluation at any time.</p>
-            <div class="last"></div>
-        </div>
+        <p><b><?php echo CHtml::encode($first->title); ?></b> scores the highest with a lead of <b><?php echo $difference; ?>%</b> compared to <b><?php  echo CHtml::encode($second->title); ?></b></p>
     </div>
-    <p><b><?php echo CHtml::encode($first->title); ?></b> scores the highest with a lead of <b><?php echo $difference; ?>%</b> compared to <b><?php  echo CHtml::encode($second->title); ?></b></p>
+    <div>
     <h3>Detailed comparison</h3>
     <p>This graph shows alternative profiles. By comparing the data points at each criteria you are able to see how each alternative compares to others. Do you still think the first one is the best?</p>
-    <div>
+        <div id="abacon-sidebar" class="sidebar">
+            <form method="post" action="">
+                <fieldset>
+                    <select name="legend">
+                        <?php foreach($Alternatives as $A) { ?>
+                            <option value="<?php echo $A->alternative_id; ?>"><?php echo CHtml::encode(Common::truncate($A->title, 32)); ?></option>
+                        <?php }?>
+                    </select>
+                    <a class="selectBox selectBox-dropdown" style="display: inline-block;" title="" tabindex="0">
+                        <span class="selectBox-label">Draw more alternatives</span>
+                        <span class="selectBox-arrow"></span>
+                    </a>
+                </fieldset>
+            </form>
+            <ul class="legend">
+                <?php $i = 0; ?>
+                <?php foreach($bestAlternatives as $A) {?>
+                    <li id="alternative_<?php echo $A->alternative_id; ?>"><span class="color" style="background-color: <?php echo $A->color; ?>">&nbsp;</span><?php echo CHtml::encode(Common::truncate($A->title, 32)); ?><span class="remove">X</span></li>
+                    <?php if($i == 1) break; ?>
+                    <?php $i++;?>
+                <?php }?>
+            </ul>
+        </div>
         <div id="abacon" class="content">
             <table class="criteria">
             <?php foreach($this->DecisionModel->findCriteriaByPriority() as $Criteria) { ?>
@@ -96,29 +119,6 @@ Graph.Data = <?php echo json_encode($eval); ?>;
                     <td>100</td>
                 </tr>
             </table>
-        </div>
-        <div id="abacon-sidebar" class="sidebar">
-            <form method="post" action="">
-                <fieldset>
-                    <select name="legend">
-                        <?php foreach($Alternatives as $A) { ?>
-                            <option value="<?php echo $A->alternative_id; ?>"><?php echo CHtml::encode(Common::truncate($A->title, 32)); ?></option>
-                        <?php }?>
-                    </select>
-                    <a class="selectBox selectBox-dropdown" style="display: inline-block;" title="" tabindex="0">
-                        <span class="selectBox-label">Draw more alternatives</span>
-                        <span class="selectBox-arrow"></span>
-                    </a>
-                </fieldset>
-            </form>
-            <ul class="legend">
-                <?php $i = 0; ?>
-                <?php foreach($bestAlternatives as $A) {?>
-                    <li id="alternative_<?php echo $A->alternative_id; ?>"><span class="color" style="background-color: <?php echo $A->color; ?>">&nbsp;</span><?php echo CHtml::encode(Common::truncate($A->title, 32)); ?><span class="remove">X</span></li>
-                    <?php if($i == 1) break; ?>
-                    <?php $i++;?>
-                <?php }?>
-            </ul>
         </div>
     </div>
 </div>
