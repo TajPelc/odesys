5efd4c9a67bbd5c085eb42deafcaa20ab95ce35e
frenk <frenk@PAMELA-VM.lan>
2011-08-28 16:04:46 +0200

    Redesigned Dashboard Minor fixes to index and public page

--- protected/views/user/dashboard.php
+++ protected/views/user/dashboard.php
@@ -1,8 +1,42 @@
 <div id="content">
     <div id="heading">
-        <h2>View your decisions and edit settings</h2>
+        <h2>Dashboard - Decision Feed</h2>
     </div>
-    <dl class="recentDecisions">
+    <ul>
+        <li class="new">
+            <img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-snc4/260658_1362051067_369722_q.jpg" title="Frenk Ten" alt="Frenk Ten" />
+            <div>
+                <h3><a href="#">Frenk Ten</a> - today 13.00</h3>
+                <p>published a decision: <a href="#">"Buying a car"</a> to <b>everyone</b>.</p>
+            </div>
+        </li>
+        <li>
+            <img src="http://profile.ak.fbcdn.net/hprofile-ak-snc4/23088_1037539823_5285_q.jpg" title="Taj Pelc" alt="Taj Pelc" />
+            <div>
+                <h3><a href="#">Taj Pelc</a> - today 12.34</h3>
+                <p>posted a <a href="#">comment</a> on your decision: <a href="#">Tig ol' Bitties"</a>.</p>
+                <blockquote>We care?</blockquote>
+            </div>
+        </li>
+        <li>
+            <img src="http://profile.ak.fbcdn.net/hprofile-ak-snc4/211944_100001775212279_7119928_q.jpg" title="Rosa Salmela" alt="Rosa Salmela" />
+            <div>
+                <h3><a href="#">Rosa Salmela</a> - today 12.12</h3>
+                <p>posted a <a href="#">comment</a> on your decision: <a href="#">Tig ol' Bitties"</a>.</p>
+                <blockquote>OMG!!! I am never hanging with you guys again! PERVERTS!</blockquote>
+            </div>
+        </li>
+        <li>
+            <img src="http://profile.ak.fbcdn.net/hprofile-ak-snc4/186124_100002179114423_3234182_q.jpg" title="Margaryta Ruzhytskaya" alt="Margaryta Ruzhytskaya" />
+            <div>
+                <h3><a href="#">Margaryta Ruzhytskaya</a> - today 12.02</h3>
+                <p>posted a <a href="#">comment</a> on your decision: <a href="#">Tig ol' Bitties"</a>.</p>
+                <blockquote>Wanna motorboat these?</blockquote>
+            </div>
+        </li>
+    </ul>
+    <a class="button" href="#">Show more<span>&nbsp;</span></a>
+    <!--dl class="recentDecisions">
         <dt>My latest decisions</dt>
         <dd>
             <table>
@@ -38,5 +72,17 @@
         <dt>My profile</dt>
         <dd class="first"><a href="#">Privacy</a></dd>
         <dd><a href="#">Settings</a></dd>
-    </dl>
+    </dl-->
+</div>
+<div id="sidebar">
+    <div class="edit">
+        <h4>Dashboard Options:</h4>
+        <ul>
+            <li><a href="#">Decision Feed</a></li>
+            <li><a href="#">Decision History</a></li>
+            <li><a href="#">Statistics</a></li>
+            <li><a href="#">Profile Settings</a></li>
+        </ul>
+        <div class="last"></div>
+    </div>
 </div>
