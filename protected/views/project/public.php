eab0b388c6446b2764f2626b3f05bd7846854ac0
frenk <frenk@PAMELA-VM.lan>
2011-10-13 19:21:32 +0200

    Removed SNS sharing when privacy not public Small design fixes

--- protected/views/project/public.php
+++ protected/views/project/public.php
@@ -89,24 +89,20 @@ Graph.Data = <?php echo json_encode($eval); ?>;
     <?php } ?>
     <div class="help">
         <h4>This decision is available to:</h4>
-        <p><b><?php echo $this->Decision->getViewPrivacyLabel(); ?></b></p>
+        <p <?php if(!$this->Decision->isPublic()) { ?>class="l"<?php } ?>><b><?php echo $this->Decision->getViewPrivacyLabel(); ?></b></p>
         <?php if(!$this->Decision->isPrivate()){ ?>
+        <?php if($this->Decision->isPublic()) { ?>
         <h4>Share on:</h4>
         <ul id="sns">
-            <?php if(($this->Decision->isOwner(Yii::app()->user->id) && $this->Decision->isFriendsOnly()) || $this->Decision->isPublic()){ ?>
-                <li>
-                    <a id="share_facebook" target="_blank" href="https://www.facebook.com/sharer.php?u=<?php echo Yii::app()->request->hostInfo; ?><?php echo CHtml::encode('/decision/'. $this->Decision->decision_id . '-' . $this->Decision->label . '.html'); ?>&amp;t=<?php echo CHtml::encode($this->Decision->title); ?>">Facebook</a>
-                </li>
-            <?php } ?>
-            <?php if($this->Decision->isPublic()) { ?>
-                <li><a id="share_twitter" target="_blank" href="https://twitter.com/share?url=<?php echo Yii::app()->request->hostInfo; ?><?php echo CHtml::encode('/decision/'. $this->Decision->decision_id . '-' . $this->Decision->label . '.html'); ?>&amp;via=ODESYSinfo">Twitter</a></li>
-                <li>
-                    <a id="share_digg" target="_blank" href="http://digg.com/submit?url=<?php echo Yii::app()->request->hostInfo; ?><?php echo CHtml::encode('/decision/'. $this->Decision->decision_id . '-' . $this->Decision->label . '.html'); ?>&amp;title=<?php echo CHtml::encode($this->Decision->title); ?>">Digg</a>
-                    <span style="display:none"><?php echo $this->Decision->description; ?></span>
-                </li>
-                <li><a id="share_reddit" target="_blank" href="http://www.reddit.com/submit?url=<?php echo Yii::app()->request->hostInfo; ?><?php echo CHtml::encode('/decision/'. $this->Decision->decision_id . '-' . $this->Decision->label . '.html'); ?>">Reddit</a></li>
-            <?php } ?>
+            <li><a id="share_facebook" target="_blank" href="https://www.facebook.com/sharer.php?u=<?php echo Yii::app()->request->hostInfo; ?><?php echo CHtml::encode('/decision/'. $this->Decision->decision_id . '-' . $this->Decision->label . '.html'); ?>&amp;t=<?php echo CHtml::encode($this->Decision->title); ?>">Facebook</a></li>
+            <li><a id="share_twitter" target="_blank" href="https://twitter.com/share?url=<?php echo Yii::app()->request->hostInfo; ?><?php echo CHtml::encode('/decision/'. $this->Decision->decision_id . '-' . $this->Decision->label . '.html'); ?>&amp;via=ODESYSinfo&amp;text=<?php echo Chtml::encode($this->Decision->title); ?>">Twitter</a></li>
+            <li>
+                <a id="share_digg" target="_blank" href="http://digg.com/submit?url=<?php echo Yii::app()->request->hostInfo; ?><?php echo CHtml::encode('/decision/'. $this->Decision->decision_id . '-' . $this->Decision->label . '.html'); ?>&amp;title=<?php echo CHtml::encode($this->Decision->title); ?>">Digg</a>
+                <span style="display:none"><?php echo $this->Decision->description; ?></span>
+            </li>
+            <li><a id="share_reddit" target="_blank" href="http://www.reddit.com/submit?url=<?php echo Yii::app()->request->hostInfo; ?><?php echo CHtml::encode('/decision/'. $this->Decision->decision_id . '-' . $this->Decision->label . '.html'); ?>">Reddit</a></li>
         </ul>
+        <?php } ?>
         <?php } ?>
         <div class="last"></div>
     </div>
