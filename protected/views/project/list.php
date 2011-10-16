c0807009960e2148fee65148c9d097ed40f341a2
frenk <frenk@PAMELA-VM.lan>
2011-10-16 04:57:40 +0200

    Added published to privacy details to the list

--- protected/views/project/list.php
+++ protected/views/project/list.php
@@ -9,14 +9,31 @@
             <th>Decision name</th>
             <th>Last modified</th>
             <th>No. opinions</th>
-            <th>Published</th>
+            <th>Published to</th>
+            <th class="l"></th>
         </tr>
         <?php foreach($Decisions as $D) { ?>
         <tr id="<?php echo $D->decision_id; ?>">
             <td><?php echo CHtml::link(CHtml::encode($D->title), '/decision/'. $D->decision_id . '-' . $D->label . '.html'); ?></td>
             <td><?php echo date('j.n.Y', strtotime($D->last_edit)); ?></td>
             <td><?php echo $D->opinionCount; ?></td>
-            <td><div><?php echo ($D->isPublished() ? 'Yes' : 'No'); ?><span>X</span></div></td>
+            <td>
+                <div>
+                    <?php 
+                    if($D->isPublished()) {
+                        if($D->isPrivate()) {
+                            echo 'Private';
+                        } elseif ($D->isFriendsOnly()) {
+                            echo 'Friends';
+                        } elseif ($D->isPublic()) {
+                            echo 'Everyone';
+                        }
+                    } else {
+                        echo '<em>Not yet</em>';
+                    }?>
+                </div>
+            </td>
+            <td class="l"><div><span>X</span></div></td>
         </tr>
         <?php } ?>
     </table>
