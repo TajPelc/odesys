6be1e06de57dcd5d65f3c9fdaab18f6a7ea62ae9
frenk <frenk@PAMELA-VM.lan>
2011-10-18 01:48:20 +0200

    Frontpage styled

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,25 +1,32 @@
+<?php $this->pageTitle=Yii::app()->name . ' | Welcome'; ?>
 <div id="heading">
     <h2>How to use Odesys in a few easy steps</h2>
 </div>
 
 <div id="content">
-    <dl>
-        <dt>What is it?</dt>
-        <dd>ODESYS is a simple online decision support system (DSS) with a friendly graphical user interface.</dd>
-        <dt>How does it work?</dt>
-        <dd>It seamlessly guides you through the process of creating a decision model, the evaluation of alternatives and results analysis. Armed with new knowledge the hard part is still up to you. Making a choice.</dd>
-        <dt>What may I use it for?</dt>
-        <dd>A wide variety of decision problems. Try it out!</dd>
-    </dl>
-    <?php $this->pageTitle=Yii::app()->name . ' How to use Odesys in a few easy steps'; ?>
-    <ol>
-        <li><p>Start by signing in with Facebook and creating a new project</p></li>
-        <li><p>Define alternatives among which you are choosing</p></li>
-        <li><p>Define criteria and order them from most to least important</p></li>
-        <li><p>Evalute each alternative by the defined criteria</p></li>
-        <li><p>Analyse the results using our graphical method</p></li>
-        <li><p>Publish your decision and invite your social circle to help</p></li>
-        <li><p>Make a better decision</p></li>
-    </ol>
+    <div>
+        <dl>
+            <dt>What is it?</dt>
+            <dd>ODESYS is a simple online decision support system (DSS) with a friendly graphical user interface.</dd>
+        </dl>
+        <dl>
+            <dt>How does it work?</dt>
+            <dd>It seamlessly guides you through the process of creating a decision model, the evaluation of alternatives and results analysis. Armed with new knowledge the hard part is still up to you. Making a choice.</dd>
+        </dl>
+        <dl>
+            <dt>What may I use it for?</dt>
+            <dd>A wide variety of decision problems. Try it out!</dd>
+        </dl>
+        <hr />
+        <ol>
+            <li><p>Start by signing in with Facebook and creating a new project</p></li>
+            <li><p>Define alternatives among which you are choosing</p></li>
+            <li><p>Define criteria and order them from most to least important</p></li>
+            <li><p>Evalute each alternative by the defined criteria</p></li>
+            <li><p>Analyse the results using our graphical method</p></li>
+            <li><p>Publish your decision and invite your social circle to help</p></li>
+            <li><p>Make a better decision</p></li>
+        </ol>
+    </div>
     <img src="../../images/introduction.png" title="kolazz" alt="kolazz" />
 </div>
