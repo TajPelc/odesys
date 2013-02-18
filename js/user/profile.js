8169b21b64843727e3c6325ec81d12f2bc9071b4
Taj Pelc <tajpelc@gmail.com>
2013-02-18 19:31:11 +0100

    My latest decisions added to the profile page.

--- js/user/profile.js
+++ js/user/profile.js
@@ -9,14 +9,6 @@ ProfileSettings = {};
  * Document Ready
  * */
 $(document).ready(function(){
-    //prepare ajax
-    url = window.location;
-    $.ajaxSetup({
-        type: 'POST',
-        url: url,
-        dataType: 'json',
-    });
-
     $('#content .buttonBig').click(function() {
         //open overlay and fill it
         Core.Overlay.Html = '<h2>Are you sure?</h2><p>You are about to delete your profile. This action is irreversible.</p><div><a href="#" class="buttonBig" id="deleteYes">Yes<span class="doors">&nbsp;</span></a><a href="#" class="buttonBig" id="deleteNo">No<span class="doors">&nbsp;</span></a></div>';
