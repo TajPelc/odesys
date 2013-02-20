fb497d87cb5fa2c7ccfe418d7dea928f7441380a
frenk <frenk@PAMELA-VM.lan>
2013-02-20 10:27:28 +0100

    Added delete functionality to decision list

--- js/user/profile.js
+++ js/user/profile.js
@@ -5,11 +5,32 @@
 
 ProfileSettings = {};
 
+DashboardList = {};
+
+DashboardList.DeleteItem = function(that) {
+    var data = {
+        'delete': that.parents('tr').attr('id'),
+    };
+    // post the form
+    $.ajax({
+        data: data,
+        success: function(data) {
+            if(data['status'] == true){
+                //here be returned shite
+                that.parents('tr').remove();
+                //errors
+            } else {
+            }
+        }
+    });
+}
+
+
 /*
  * Document Ready
  * */
 $(document).ready(function(){
-    $('#content .buttonBig').click(function() {
+    $('#delete').click(function() {
         //open overlay and fill it
         Core.Overlay.Html = '<h2>Are you sure?</h2><p>You are about to delete your profile. This action is irreversible.</p><div><a href="#" class="buttonBig" id="deleteYes">Yes<span class="doors">&nbsp;</span></a><a href="#" class="buttonBig" id="deleteNo">No<span class="doors">&nbsp;</span></a></div>';
         Core.Overlay(Core.Overlay.Html);
@@ -33,4 +54,33 @@ $(document).ready(function(){
         });
         return false;
     });
+
+    //prepare ajax
+    url = '/user/decisions/';
+    $.ajaxSetup({
+        type: 'POST',
+        url: url,
+        dataType: 'json'
+    });
+    $('table .delete').click(function() {
+        //remember decision object
+        DashboardList.Item = $(this);
+
+        //open overlay and fill it
+        Core.Overlay.Html = '<h2>Are you sure?</h2><p>You are about to delete decision model named "'+DashboardList.Item.parents('td').siblings('td:first-child').text()+'". This action is irreversible.</p><div><a href="#" class="buttonBig" id="deleteYes">Yes<span class="doors">&nbsp;</span></a><a href="#" class="buttonBig" id="deleteNo">No<span class="doors">&nbsp;</span></a></div>';
+        Core.Overlay(Core.Overlay.Html);
+
+
+        //handle delete action
+        $('#deleteYes').click(function(){
+            DashboardList.DeleteItem(DashboardList.Item);
+            Core.Overlay.Close();
+        });
+        $('#deleteNo').click(function(){
+            Core.Overlay.Close();
+        });
+        return false;
+    });
+
+
 });
