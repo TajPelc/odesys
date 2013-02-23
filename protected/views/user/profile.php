28eeacde2ce42e9c6ca3ae0349507977a83f6325
frenk <frenk@PAMELA-VM.lan>
2013-02-23 12:14:38 +0100

    Fixes to abacon sidebar. Fixed wrong link to new decision on user profile

--- protected/views/user/profile.php
+++ protected/views/user/profile.php
@@ -2,7 +2,7 @@
 <section class="content">
     <h1>Welcome <b><?php echo substr(Yii::app()->user->name, 0, strpos(Yii::app()->user->name, ' ')); ?></b>, thank you for logging in with your social account. You may now use all the features of odesys.</h1>
     <div>
-        <?php echo CHtml::link('create a new decision', array('/site/login/'), array('class'=>'decisionNew', 'title'=>'create a new decision')); ?>
+        <?php echo CHtml::link('create a new decision', array('/project/create/'), array('class'=>'decisionNew', 'title'=>'create a new decision')); ?>
     </div>
 </section>
 <section class="accounts">
