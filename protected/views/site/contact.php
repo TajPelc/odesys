da9c3eb3eb8ab118b460abc0dc986eabfac3e96e
frenk <frenk@PAMELA-VM.lan>
2014-11-11 12:40:41 +0100

    Fixed youtube video. Updated contact info. Added paypal donation button.

--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -6,17 +6,16 @@
         <a target="_blank" class="twitter" href="https://twitter.com/DecisionTool">Twitter</a>
         <a target="_blank" class="google" href="https://plus.google.com/112275384094460979880/">Google+</a>
     </div>
-    <p>You can also do it the old fashion way - via <?php if(Yii::app()->user->isGuest){ ?><b>info -[at]- odesys.info</b><?php } else { ?><a href="mailto:info@odesys.info">info@odesys.info</a><?php } ?></p>
     <h1>Follow the developers</h1>
     <div id="developers">
         <dl>
             <dt><img src="https://graph.facebook.com/tajpelc/picture" title="Taj Pelc" alt="Taj Pelc" />Taj Pelc</dt>
-            <dd><?php if(Yii::app()->user->isGuest){ ?><b>taj.pelc -[at]- odesys.info</b><?php } else { ?><a href="mailto:taj.pelc@odesys.info">taj.pelc@odesys.info</a><?php } ?></dd>
+            <dd>Backend Developer</dd>
             <dd><a href="https://www.facebook.com/tajpelc"  title="Taj Pelc on Facebook">Facebook</a> | <a href="https://twitter.com/#!/tajpelc"  title="Taj Pelc on Twitter">Twitter</a> | <a href="https://plus.google.com/115350672233878010427/"  title="Taj Pelc on Google+">Google+</a> | <a href="http://linkedin.com/in/tajpelc" title="Taj Pelc on LinkedIn">LinkedIn</a></dd>
         </dl>
         <dl>
             <dt><img src="https://graph.facebook.com/frenk.ten/picture" title="Frenk T. Sedmak Nahtigal" alt="Frenk T. Sedmak Nahtigal" />Frenk T. Sedmak Nahtigal</dt>
-            <dd><?php if(Yii::app()->user->isGuest){ ?><b>frenk.ten -[at]- odesys.info</b><?php } else { ?><a href="mailto:frenk.ten@odesys.info">frenk.ten@odesys.info</a><?php } ?></dd>
+            <dd>Frontend Developer</dd>
             <dd><a href="https://www.facebook.com/frenk.ten"  title="Frenk Ten on Facebook">Facebook</a> | <a href="https://twitter.com/#!/frenkten"  title="Frenk Ten on Twitter">Twitter</a> | <a href="https://plus.google.com/113782485154610601594/"  title="Frenk Ten on Google+">Google+</a> | <a href="http://linkedin.com/in/frenkten" title="Frenk Ten on LinkedIn">LinkedIn</a></dd>
         </dl>
     </div>
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -26,26 +26,34 @@
                 <?php echo Yii::t('frontPage','<a href="{url}" title="begin your journey" class="decisionNew">make a decision</a>', array('{url}' => '/project/create/')); ?>
             <?php } ?>
         </div>
-        <iframe id="promoVideo" width="920" height="518" onload="floaded()" src="https://www.youtube.com/embed/31pLgeOdohg?rel=0&hd=1&vq=hd720&wmode=transparent&showinfo=0" frameborder="0" allowfullscreen></iframe>
-
         <script>
             //onYouTubePlayerAPIReady
-            $('#promoVideo').attr('height', 200);
+            function promoVideoStateChange(event){
+                if (event.data == YT.PlayerState.PLAYING) {
+                    $('#promoVideo').attr('height', 518);
+                    promoVideo.setPlaybackQuality('hd720');
+                    promoVideo.getOptions('cc');
+                }
+                else if (event.data == YT.PlayerState.ENDED) {
+                    $('#promoVideo').attr('height', 200);
+                    promoVideo.stopVideo();
+                }
+            }
             var promoVideo;
             function floaded(){
                 promoVideo = new YT.Player('promoVideo', {
+                    height: '200',
                     videoId: '31pLgeOdohg',
+                    setPlaybackQuality: 'hd720',
                     events:
                     {
-                        'onStateChange': function (event)
-                        {
-                            if (event.data == YT.PlayerState.PLAYING)      { $('#promoVideo').attr('height', 518); }
-                            else if (event.data == YT.PlayerState.ENDED)   { $('#promoVideo').attr('height', 200); promoVideo.stopVideo(); }
-                        }
+                        'onStateChange': promoVideoStateChange
                     }
                 });
             }
         </script>
+        <iframe id="promoVideo" width="920" onload="floaded()" src="https://www.youtube.com/embed/31pLgeOdohg?rel=0&hd=1&vq=hd720&wmode=transparent&showinfo=0&enablejsapi=1&cc_load_policy=1" frameborder="0" allowfullscreen></iframe>
+
 
         <ul class="btcf" id="threeMusketeers">
             <li>
@@ -55,8 +63,14 @@
                     </header>
                     <p><?php echo Yii::t('frontPage','We are decision theory researches and explorers of the human mind.'); ?></p>
                     <p><?php echo Yii::t('frontPage','By combining our knowledge of decision theory with our skills of web development, we would like to create a world where the quality of our decisions and the quality of life are at their best.'); ?></p>
-                    <p><?php echo Yii::t('frontPage','We are always looking for new connections.'); ?></p>
-                    <p><?php echo Yii::t('frontPage','If you would like to work with us or have a question, we encourage you to <a href="{url}" title="contact us">get in touch</a>.', array('{url}' => '/site/contact/')); ?></p>
+                    <p><?php echo Yii::t('frontPage','We will keep on supporting and developing odesys, but we need your help. By <a href="{url}" title="make a donation">making a donation</a> you will ensure further support for this project.', array('{url}' => 'https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=PKZUNC95HS3FY&lc=US&item_name=Odesys%20%2f%2f%20helping%20you%20decide&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted')); ?></p>
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                        <input type="hidden" name="cmd" value="_s-xclick">
+                        <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHPwYJKoZIhvcNAQcEoIIHMDCCBywCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBjSdIqAtgQfdDmwFopDoK0zhZYIhYU/WRofhnUBY08lN8OHWewMRI8ipl/vWMKcQH6tS05jEpaZk88NKfywNSmhQrNJp6Wbc293/asNVhk1qlRQVSXfeUDhO93YFieX9dN50KXoWVbSBFWHiRLQ3yUFE1G54i5EpmPsSuLj66ydTELMAkGBSsOAwIaBQAwgbwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIiAQNt6aNlvOAgZgFEmQXjDZAYnG4kSn4DMxk5mM4mS2CQjpMm1Hzz9kXIyb4d0Rh7dVCVyI95uMChYdWKJWUcomC4tGknY1TqVho0IEExbjRG90ajPX0I8QC+0n4IhoOCaB0oP4J26FkecdbFu2RSbx6Z0fRt2fanRJg6vlTkpO8fFPsylkGTYg51quaucTDX4P88RhooP5oRqD6imXEFxMqBaCCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTE0MTExMTEwMTI1NVowIwYJKoZIhvcNAQkEMRYEFKaP9YUeTC9A+/nGos5O2b5qm7DRMA0GCSqGSIb3DQEBAQUABIGAAiSe/Gpy/uCCk5tbQRw/KeZxKsiutb+K6Zfcf0y5gcFgngYYNzIZvVruoOHdHyy66a/wagjDUQDV7/rinX7sgYN76yWsUDBVueocnU58tcAOQNKAuH+amB8G8jE+SV/ajj1XBGSMpbSLBRItLmTS38zkgmhNVFfhvgqrovm8KWM=-----END PKCS7-----
+">
+                        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                    </form>
                 </article>
             </li>
             <li>
